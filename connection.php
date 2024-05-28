0e7342c5101129b5e6db7d44e6b61ef402cafb53
Itekadon <163440879+Itekadon@users.noreply.github.com>
2024-05-28 02:12:12 +0200

    connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "id22230718_digital_dynasty";
+$dbname = "digital_dynasty";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
